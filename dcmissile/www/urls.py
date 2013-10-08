de2068e8d0d90a8400cb91e4d41deca84ab8d720
Gilberto Gonçalves <lursty@gmail.com>
2013-10-08 20:27:28 +0100

    Removed unused import
    And added pyusb to requirements

--- dcmissile/www/urls.py
+++ dcmissile/www/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('dcmissile.www.views',
     url(r'^$', 'home', name="home"),
